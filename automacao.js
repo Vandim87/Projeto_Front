efe2c54f75854bdf699efb1edda5ccb2bc243825
Vandim87 <107519365+Vandim87@users.noreply.github.com>
2022-08-10 20:09:10 -0300

    5

--- automacao.js
+++ automacao.js
@@ -1,6 +1,9 @@
-/*var mensagem = "óla";
-mensagem = 'óla Evandro';
-console.log(mensagem)*/
-function imprimeOla(){
-    console.log('Óla');
-}
+var n1 = document.getElementById('n1');
+var n2 = document.getElementById('n2');
+var r = document.getElementById('r');
+
+function calcular (){
+    var resultado = n1.valueAsNumber + n2.valueAsNumber;
+    if (isNaN(resultado))
+    r.textContent = resultado;
+    }
